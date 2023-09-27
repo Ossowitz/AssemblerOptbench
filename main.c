b611aebb31b6e6727aa0de50c03d0143ea2cffb8
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-09-27 21:05:55 +0300

    Refactoring

--- main.c
+++ main.c
@@ -5,13 +5,5 @@
 int j5, k5, i5;
 
 int main() {
-    j5 =0;
-    do {
-        k5 = k5 - 1;
-        j5 = j5 + 1;
-        i5 = (k5 * 3) / (j5 * constant5);
-    } while (k5 > 0);
-    printf("%d", i5);
-    printf("%d", k5);
-    printf("%d", j5);
+
 }
