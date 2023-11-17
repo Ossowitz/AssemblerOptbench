8339b9b3180e07d6020e284398fbcdb9ba9b2737
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-11-18 00:21:02 +0300

    Clean code for incOpt.asm

--- source/deepSubexpressions/nonOpt.asm
+++ source/deepSubexpressions/nonOpt.asm
@@ -1,16 +1,19 @@
 L9:
-        mov     eax, DWORD PTR i[rip]
-        cmp     eax, 9
-        jg      .L11
-        mov     edx, DWORD PTR i5[rip]
-        mov     eax, DWORD PTR i2[rip]
-        add     eax, edx
-        mov     DWORD PTR j5[rip], eax
-        jmp     .L12
-.L11:
-        mov     edx, DWORD PTR i5[rip]
-        mov     eax, DWORD PTR i2[rip]
-        add     eax, edx
-        mov     DWORD PTR k5[rip], eax
-.L12:
-        mov     eax, 0
+    mov     eax, DWORD [rip+i]
+    cmp     eax, 9
+    jg      L11
+
+    mov     edx, DWORD [rip+i5]
+    mov     eax, DWORD [rip+i2]
+    add     eax, edx
+    mov     DWORD [rip+j5], eax
+    jmp     L12
+
+L11:
+    mov     edx, DWORD [rip+i5]
+    mov     eax, DWORD [rip+i2]
+    add     eax, edx
+    mov     DWORD [rip+k5], eax
+
+L12:
+    mov     eax, 0
