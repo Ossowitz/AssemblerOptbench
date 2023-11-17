1fbec18b53bf30536d793409a501149857f5ef06
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-11-18 00:16:37 +0300

    Clean code for incOpt.asm

--- source/circleFailure/incOpt.asm
+++ source/circleFailure/incOpt.asm
@@ -1,20 +1,19 @@
 main:
         endbr64
-        # Перемещаем значение из памяти в регистр
-        #   Значение загружается из локации .LC0
-        mov     rax, QWORD PTR .LC0[rip]  # tmp84,
-        # Установка значения 1 по смещению 8 от начала массива
-        mov     DWORD PTR ivector[rip+8], 1       # ivector[2],
-        # Установка значения 3 переменной i
-        mov     DWORD PTR i[rip], 3       # i,
-        # Перемещение значения из регистра rax в начало массива
-        mov     QWORD PTR ivector[rip], rax       # MEM <...> [(int *)&ivector], tmp84
-        xor     eax, eax  #
+
+        mov     rax, QWORD PTR .LC0[rip]
+        mov     DWORD PTR ivector[rip+8], 1
+        mov     DWORD PTR i[rip], 3
+        mov     QWORD PTR ivector[rip], rax
+        xor     eax, eax
         ret
+
 i:
         .zero   4
+
 ivector:
         .zero   12
+
 .LC0:
         .long   1
         .long   1
